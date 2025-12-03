f9bd4b0c16a2f04a19dbbe204dcbec22bf9825e9
Sisay G <sisay@Aragaws-Air.lan>
2025-12-03 12:14:45 -0500

    Fix API setup

---
+++ netflix-clone/src/components/Utils/Axios.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import axiosInstance from "../axiosInstance";
+import requests from "../requests";
+
+function Movies() {
+  const [trending, setTrending] = useState([]);
+
+  useEffect(() => {
+    const fetchTrending = async () => {
+      try {
+        const response = await axiosInstance.get(requests.fetchTrending);
+        setTrending(response.data.results);
+      } catch (error) {
+        console.error("Error fetching trending movies:", error);
+      }
+    };
+
+    fetchTrending();
+  }, []);
+
+  return (
+    <div>
+      <h2>Trending Movies & Shows</h2>
+      <ul>
+        {trending.map((movie) => (
+          <li key={movie.id}>{movie.title || movie.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Movies;
