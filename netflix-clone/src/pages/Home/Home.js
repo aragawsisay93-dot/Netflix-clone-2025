a0c197944a4e336a74e2dbd9fcb4097b2e391cfe
Sisay G <sisay@Aragaws-Air.lan>
2025-12-03 15:51:03 -0500

    Update Home.js with RowList component

--- netflix-clone/src/pages/Home/Home.js
+++ netflix-clone/src/pages/Home/Home.js
@@ -2,14 +2,16 @@ import React from "react";
 import Header from "../../components/Header/Header.js"
 import Footer from "../../components/Footer/Footer.js";
 import Banner from "../../components/Banner/Banner"; 
+import RowList from "../../components/Rows/RowList/RowList"; 
 
 const Home = ()=>{
-    return(
-        <>
-           <Header/>
-           <Banner/>
-           <Footer/>
-        </>
-    )
+    return (
+      <>
+        <Header />
+        <Banner />
+        <RowList />
+        <Footer />
+      </>
+    );
 }
 export default Home
